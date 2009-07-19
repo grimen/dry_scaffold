2ff250ddc1f4c9fcaa4e02024a02c237323c3817
Jonas Grimfelt <grimen@gmail.com>
2009-07-19 18:21:11 -0400

    Fixed incorrect test_helper path causing tests to fail. =P

--- generators/dry_model/templates/models/tests/test_unit/unit_test.rb
+++ generators/dry_model/templates/models/tests/test_unit/unit_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper'))
 
 class <%= class_name %>Test < ActiveSupport::TestCase
   
--- generators/dry_scaffold/templates/helpers/tests/test_unit/unit_test.rb
+++ generators/dry_scaffold/templates/helpers/tests/test_unit/unit_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'test_helper'))
  
 class <%= controller_class_name %>HelperTest < ActionView::TestCase
   
